6013d9e7181ad40854e8b8e38e14075c72738b1c
András Veres-Szentkirályi <vsza@vsza.hu>
2013-05-22 11:06:54 +0200

    added TCPDF example

---
+++ tcpdf_example.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from php import PHP
+
+def main():
+	php = PHP('php')
+	php.include('tcpdf/config/lang/eng.php')
+	php.include('tcpdf/tcpdf.php')
+
+	pdf = php.new('TCPDF', 'P', 'mm', 'A4', True, 'UTF-8')
+	
+	pdf.setFontSubsetting(False)
+	pdf.setAuthor('VSzA')
+	pdf.setCreator('PPCG')
+	pdf.setTitle('TCPDF in Python!?')
+	pdf.setPrintHeader(False)
+	pdf.setMargins(10, 10)
+
+	pdf.AddPage()
+	pdf.SetFont('dejavusans', 'B', 12)
+	pdf.Cell(0, 0, 'Hello Python')
+	pdf.Ln()
+
+	x = pdf.GetX()
+	y = pdf.GetY()
+	pdf.setXY(30, 30)
+	pdf.Cell(0, 0, 'GOTOs are bad')
+	pdf.setXY(x, y)
+	pdf.Cell(0, 0, 'I can has PHP variables')
+
+	pdf.Output()
+	pdf_data = str(php)
+
+	with file('output.pdf', 'wb') as pdf_file:
+		pdf_file.write(pdf_data)
+	print '{0} bytes of PDF have been written'.format(len(pdf_data))
+
+
+if __name__ == '__main__':
+	main()
